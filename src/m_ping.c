d152d3780f268342e2b4ed661d6b1f1606bf3788
ejb <ejb>
2001-02-11 08:00:14 +0000

    - added Id tags, removed unused parse.c

--- src/m_ping.c
+++ src/m_ping.c
@@ -1,3 +1,23 @@
+/************************************************************************
+ *   Copyright (C) 2000,2001 Edward Brocklesby
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 1, or (at your option)
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * $Id: m_ping.c,v 1.2 2001/02/11 08:00:21 ejb Exp $
+ */
+
 #include "clients.h"
 #include "handlers.h"
 #include "send.h"
@@ -5,6 +25,6 @@
 int
 m_ping(struct Client *cptr, struct Client *sptr, int parc, char **parv)
 {
-	sendto_one(cptr, "PONG :%s", parv[1]);
+	sendto_one(sptr, "PONG :%s", parv[1]);
 	return 0;
 }
--- src/tools.c
+++ src/tools.c
@@ -1,11 +1,29 @@
+/************************************************************************
+ *   Copyright (C) 2000,2001 Edward Brocklesby
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 1, or (at your option)
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * $Id: tools.c,v 1.2 2001/02/11 08:00:23 ejb Exp $
+ */
+
 /*
  * tools.c
  * 
  * Useful stuff, ripped from places ..
  *
  * adrian chadd <adrian@creative.net.au>
- *
- * $Id: tools.c,v 1.1 2001/02/11 07:46:39 ejb Exp $
  */
 
 #include <assert.h>
